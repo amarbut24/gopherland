32ad8c42270e9bca89fe3d5563892d6872b06a54
Anthony <anthonymarbut24@outlook.com>
2022-04-09 17:17:37 -0700

    remove script file

--- auth/env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export AZURE_TENANT_ID="02e9f3a0-53a5-4898-bb6e-e97008b17be7"
-export AZURE_CLIENT_ID="98b51714-780b-41ab-b0a9-aaa8833b6be2"
-export AZURE_CLIENT_CERTIFICATE_PATH="/etc/ssl/private/selfsigned.key"
