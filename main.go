b24577ecc9a20237d838e1db775f1d840cdc5999
Anthony <anthonymarbut24@outlook.com>
2022-04-10 15:05:51 -0700

    more user dev

--- main.go
+++ main.go
@@ -4,46 +4,31 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/amarbut24/gopherland/auth"
-	a "github.com/microsoft/kiota-authentication-azure-go"
-	msgraphsdk "github.com/microsoftgraph/msgraph-sdk-go"
+	"github.com/amarbut24/gopherland/users"
 )
 
 var envvars = make(map[string]string)
 
 func main() {
-	// set env vars
+	//Set env vars for NewDefaultAzureCredential
 	envvars["AZURE_TENANT_ID"] = "02e9f3a0-53a5-4898-bb6e-e97008b17be7"
 	envvars["AZURE_CLIENT_ID"] = "98b51714-780b-41ab-b0a9-aaa8833b6be2"
 	envvars["AZURE_CLIENT_CERTIFICATE_PATH"] = "/home/anthony/selfsigned.crt"
 	auth.SetAzureEnv(envvars)
 
-	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	log.Printf("Creating msgraph client")
+	client, err := auth.AzureGraphClient()
 	if err != nil {
-		log.Fatalf("Authentication failure: %+v", err)
+		log.Fatalf("unable to create msgraph client with error: %v", err)
 	}
 
+	u, err := users.GetUserByID(client, "8e086b41-7bc0-4b4a-9c3e-0a7ff59d710b")
 	if err != nil {
-		fmt.Printf("Error creating credentials: %v\n", err)
+		log.Fatalf("unable to locate user: %v", err)
 	}
-
-	auth, err := a.NewAzureIdentityAuthenticationProviderWithScopes(cred, []string{"Files.Read"})
-	if err != nil {
-		fmt.Printf("Error authentication provider: %v\n", err)
-		return
-	}
-
-	adapter, err := msgraphsdk.NewGraphRequestAdapter(auth)
-	if err != nil {
-		fmt.Printf("Error creating adapter: %v\n", err)
-		return
-	}
-	client := msgraphsdk.NewGraphServiceClient(adapter)
-
-	result, err := client.Me().Drive().Get(nil)
-	if err != nil {
-		fmt.Printf("Error getting the drive: %v\n", err)
-	}
-	fmt.Printf("Found Drive : %v\n", *result.GetId())
+	fmt.Println(*u.GetUserPrincipalName())
+	title := "CEO"
+	u.SetJobTitle(&title)
+	fmt.Println(*u.GetJobTitle())
 }
