076a890a659c7e14183d53f9c5794b0615e5c3c6
Anthony <anthonymarbut24@outlook.com>
2022-05-31 18:12:03 -0700

    add second test

--- gopherusers/users_test.go
+++ gopherusers/users_test.go
@@ -4,13 +4,13 @@ import (
 	"testing"
 
 	"github.com/amarbut24/gopherland/auth"
+	models "github.com/microsoftgraph/msgraph-sdk-go/models"
 )
-
-var envvars = make(map[string]string)
 
 //Uncomment below block if you need to run test locally
 /*
 func init() {
+	envvars := make(map[string]string)
 	envvars["AZURE_TENANT_ID"] = "02e9f3a0-53a5-4898-bb6e-e97008b17be7"
 	envvars["AZURE_CLIENT_ID"] = "98b51714-780b-41ab-b0a9-aaa8833b6be2"
 	envvars["AZURE_CLIENT_CERTIFICATE_PATH"] = "/home/anthony/selfsigned.crt"
@@ -18,12 +18,9 @@ func init() {
 }
 */
 
-func TestClient(t *testing.T) {
-	_, _, err := auth.AzureGraphClient()
-	if err != nil {
-		t.Errorf("unable to authenticate to azure ad %v", err)
-	}
-}
+//testUser will be populated vai TestNewUser
+//and will be used throughout the remaining tests
+var testUser models.Userable
 
 func TestNewUser(t *testing.T) {
 	u1 := GopherUser{
@@ -48,14 +45,19 @@ func TestNewUser(t *testing.T) {
 		t.Logf("existing user %v was found when attempting to create new user", u1.UserPrincipalName)
 	} else {
 		t.Logf("created user %s\n", *newUser.GetUserPrincipalName())
+		testUser = newUser
 	}
 }
 
-// func ExampleGetUserByID() {
-// 	client, err := auth.AzureGraphClient()
-// 	u, err := GetUserByID(client, "8e086b41-7bc0-4b4a-9c3e-0a7ff59d710b")
-// 	if err != nil {
-// 		log.Fatalf("unable to locate user: %v", err)
-// 	}
-// 	// Output: olleh
-// }
+func TestGetUserByID(t *testing.T) {
+	client, _, err := auth.AzureGraphClient()
+	if err != nil {
+		t.Errorf("unable to authenticate to azure ad %v", err)
+	}
+
+	byID, err := GetUserByID(client, *testUser.GetId())
+	if err != nil {
+		t.Errorf("unable to locate user: %v", err)
+	}
+	t.Logf("found user based on Id = %v", *byID.GetId())
+}
